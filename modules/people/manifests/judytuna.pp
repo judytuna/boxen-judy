152510c57d41af4c5b652f794df1d047eb48a202
Judy Tuan <judytuna@gmail.com>
2013-07-01 11:57:10 -0700

    personal manifest

---
+++ modules/people/manifests/judytuna.pp
@@ -0,0 +1,21 @@
+class people::judytuna {
+  include hipchat
+  include alfred
+  include postgresql
+  include chrome
+  include rubymine
+  include league_of_legends
+  include adium
+  include macvim
+  include dropbox
+  include minecraft
+  include xscope
+  include rdio
+  include spotify
+  include heroku
+  include firefox
+  include sourcetree
+  include imagemagick
+  include virtualbox
+  include phantomjs
+}
